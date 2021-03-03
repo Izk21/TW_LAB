756ff96c08864c9a97d2cd8a94ead53cc17c37f6
Elena-Izabela Lungu <73034249+Izk21@users.noreply.github.com>
2021-03-03 22:32:18 +0200

    Add files via upload
    CV page

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function fPrincipala(){alert("hello");}
+window.onload=fPrincipala;
+window.onload=function ()
+{/*alert("hello1");*/
+var I= document.getElementById("I1");
+//alert(I.src);
+alert(I.id);
+}
